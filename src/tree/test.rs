8d9d4558e006b6a80114fa57be494416dae431ba
insanitybit <insanitybit@gmail.com>
2016-04-30 13:22:09 -0400

    Add bench

--- src/tree/test.rs
+++ src/tree/test.rs
@@ -13,35 +13,35 @@ mod tests {
 
 
     #[test]
-     fn test_gini_impurity() {
-         let impurity = DecisionTree::gini_impurity(0.5, 0.5, 0.5);
-         let expected = 0.5;
-         assert!(impurity == expected);
+    fn test_gini_impurity() {
+        let impurity = DecisionTree::gini_impurity(0.5, 0.5, 0.5);
+        let expected = 0.5;
+        assert!(impurity == expected);
 
-         let impurity = DecisionTree::gini_impurity(0.8, 0.4, 0.6);
-         let expected = 0.48;
-         assert!(impurity == expected);
+        let impurity = DecisionTree::gini_impurity(0.8, 0.4, 0.6);
+        let expected = 0.48;
+        assert!(impurity == expected);
 
-         let impurity = DecisionTree::gini_impurity(0.3, 1.0, 0.0);
-         let expected = 0.0;
-         assert!(impurity == expected);
-        }
+        let impurity = DecisionTree::gini_impurity(0.3, 1.0, 0.0);
+        let expected = 0.0;
+        assert!(impurity == expected);
+    }
 
 
     #[test]
-    fn test_split(){
-        let X = OwnedArray::random((10,5), Range::new(0.,10.));
+    fn test_split() {
+        let X = OwnedArray::random((10, 5), Range::new(0., 10.));
         let feature_idx = 4;
         let value = 4.0;
-        let (left, right) = DecisionTree::split(X.column(feature_idx),value);
-        assert!(left.iter().all(|&x| X.get((x,feature_idx)).unwrap() <= &value));
-        assert!(right.iter().all(|&x| X.get((x,feature_idx)).unwrap() > &value))
+        let (left, right) = DecisionTree::split(X.column(feature_idx), value);
+        assert!(left.iter().all(|&x| X.get((x, feature_idx)).unwrap() <= &value));
+        assert!(right.iter().all(|&x| X.get((x, feature_idx)).unwrap() > &value))
     }
 
     #[test]
-    fn test_calculate_split(){
+    fn test_calculate_split() {
 
-        let X = arr2(&[[-8.0], [-2.0], [0.0], [0.0],[0.0],[0.2],[1.0]]);
+        let X = arr2(&[[-8.0], [-2.0], [0.0], [0.0], [0.0], [0.2], [1.0]]);
 
         let y = OwnedArray::from_vec(vec![1.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]);
 
@@ -52,9 +52,9 @@ mod tests {
     }
 
     #[test]
-    fn test_calculate_split_1(){
+    fn test_calculate_split_1() {
 
-        let X = arr2(&[[-8.0], [-2.0], [0.0], [0.0],[0.0],[0.2],[1.0]]);
+        let X = arr2(&[[-8.0], [-2.0], [0.0], [0.0], [0.0], [0.2], [1.0]]);
 
         let y = OwnedArray::from_vec(vec![1.0, 1.0, 1.0, 1.0, 1.0, 0.0, 0.0]);
 
@@ -68,9 +68,10 @@ mod tests {
     #[test]
     fn test_tree_building() {
 
-        let X = arr2(&[[0.0, 1.0], [1.0,0.0],[1.0,0.0],[1.0,0.0],[1.0,0.0],[0.0, 1.0],[0.0, 1.0]]);
+        let X = arr2(&[[0.0, 1.0], [1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [1.0, 0.0], [0.0, 1.0],
+                       [0.0, 1.0]]);
 
-        let y = OwnedArray::from_vec(vec![1.0, 0.0,0.0,0.0,0.0,1.0,1.0]);
+        let y = OwnedArray::from_vec(vec![1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0]);
 
         let mut dt = DecisionTree::new();
 
@@ -78,35 +79,59 @@ mod tests {
 
         let pred = dt.predict(&X).ok().unwrap();
 
-        assert!( y.all_close(&pred, 1e-8));
+        assert!(y.all_close(&pred, 1e-8));
 
     }
-    
+
     #[test]
-    fn break_tree(){
+    fn break_tree() {
         let rows = 500;
         let cols = 20;
 
-        let X = OwnedArray::random((rows,cols), Range::new(0.,10.));
+        let X = OwnedArray::random((rows, cols), Range::new(0., 10.));
         let mut rng = thread_rng();
         let y = OwnedArray::from_vec((0..rows)
-                                .map(|_| *rng.choose(&vec![0.0, 1.0][..]).unwrap())
-                                .collect::<Vec<_>>());
+                                         .map(|_| *rng.choose(&vec![0.0, 1.0][..]).unwrap())
+                                         .collect::<Vec<_>>());
 
         let mut dt = DecisionTree::new();
-        dt.fit(&X,&y);
+        dt.fit(&X, &y);
     }
+
+
+    #[bench]
+    fn bench_predict_tree(b: &mut Bencher) {
+
+        let rows = 20;
+        let cols = 20;
+
+        let X = OwnedArray::random((rows, cols), Range::new(0., 10.));
+        let mut rng = thread_rng();
+        let y = OwnedArray::from_vec((0..rows)
+                                         .map(|_| *rng.choose(&vec![0.0, 1.0][..]).unwrap())
+                                         .collect::<Vec<_>>());
+
+        let mut dt = DecisionTree::new();
+
+        dt.fit(&X, &y);
+
+        b.iter(|| {
+            dt.predict(&X);
+        });
+    }
+
+
     #[bench]
     fn bench_tree(b: &mut Bencher) {
 
         let rows = 20;
         let cols = 20;
 
-        let X = OwnedArray::random((rows,cols), Range::new(0.,10.));
+        let X = OwnedArray::random((rows, cols), Range::new(0., 10.));
         let mut rng = thread_rng();
         let y = OwnedArray::from_vec((0..rows)
-                                .map(|_| *rng.choose(&vec![0.0, 1.0][..]).unwrap())
-                                .collect::<Vec<_>>());
+                                         .map(|_| *rng.choose(&vec![0.0, 1.0][..]).unwrap())
+                                         .collect::<Vec<_>>());
 
         let mut dt = DecisionTree::new();
 
